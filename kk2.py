9587a32c9a193919b50683a464e287862e78a058
karthika06012004-hub <karthika06012004@gmail.com>
2025-11-07 15:59:23 +0530

    Add files via upload

---
+++ kk2.py
@@ -0,0 +1,32 @@
+if 5<4:
+    print("N")
+if 4<5:
+    print("Y")
+if"name"=="NAME":
+    print("O")
+if 10%2==0:#what does % do?
+    print("E")
+if 10%3==0:
+    print("T")
+if(4*6)==24:
+    print("s")
+
+
+
+
+food=input("what's the best food?.")
+
+
+if food=="rice":
+    print("yes,rice is the best.")
+
+
+elif food=="apple":
+    print("apples aren't my cup of rice.")
+else:
+    print("never heard of it!
+
+
+
+
+
