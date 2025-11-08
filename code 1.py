85c5c393fe6164d6beab33670e5980efd9ff0a0a
karthika06012004-hub <karthika06012004@gmail.com>
2025-11-08 17:29:58 +0530

    Add files via upload

---
+++ code 1.py
@@ -0,0 +1,34 @@
+if 5<4:
+    print("N")
+if 4<5:
+    print("y")
+if"name"=="name":
+    print("o")
+if 10%2==0:# what does%do?
+    print("E")
+if 10%3==0:
+    print("T")
+if(4*6)==24:
+    print("S")
+
+
+food=input("what's the best food")
+
+if food=="rice":
+   print("yes rice is the best")
+
+
+
+elif food=="apple":
+    print("Apple aren't my cup of rice.")
+else:
+    print("never heart of rice.")
+
+
+limit=12
+
+counter=1
+while counter<limit:
+   print(counter)
+   counter=counter+1
+
